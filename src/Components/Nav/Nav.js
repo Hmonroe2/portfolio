4fb4e3545f55364513451eff26b44b1dda30d5a7
Hunter Monroe <huntis@outlook.com>
2023-02-18 18:02:40 -0700

    Added a change text function

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../Nav/Nav.css'
 function Nav() {
+  const [aboutText, setAboutText] = useState('About')
+  const [link, setLink] = useState('/About')
+  
+  const changeText = () => {
+    setAboutText('Home')
+    setLink('/')
+    changeLink()
+  }
+  const changeLink = () => {
+    if (link !== '/About') {
+      setAboutText('About')
+      setLink('/About')
+    }
+  }
+
   return (
     <div>
-      <Link to="/About">
-        <button> About </button>
+      <Link to={link}>
+        <button onClick={() => { changeText() }}> {aboutText} </button>
       </Link>
       <button> Work Experience </button>
       <button> Contact</button>
